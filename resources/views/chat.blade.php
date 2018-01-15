01c7c4616434bed398a8dae9019c069a0aa42d73
Samuel Terra <samuelterra22@hotmail.com>
2018-01-15 00:37:29 -0200

    Commit inicial.

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Chatroom</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Style-->
+    <link rel="stylesheet" href="css/app.css">
+
+<body>
+
+    <div id="app">
+        <h1>Chatroom</h1>
+        <example-component></example-component>
+    </div>
+
+    <script src="js/app.js" charset="utf-8"></script>
+
+</body>
+</html>
